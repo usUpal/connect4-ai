3ff8ce2d2027a889d43377fc8f37428d8efa110a
usUpal <udyan.upal@gmail.com>
2022-04-04 20:51:40 +0600

    vertical wining move not working

--- connect4.py
+++ connect4.py
@@ -8,14 +8,14 @@ COLUMN_COUNT = 7
 
 
 def create_board():
-    board = np.zeros((6,7))
+    board = np.zeros((ROW_COUNT,COLUMN_COUNT))
     return board
 
 def drop_piece(board, row, col, piece):
     board[row][col]  = piece
 
 def is_valid_location(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT-1][col] == 0
 
 def get_next_open_row(board, col):
     for r in range(ROW_COUNT):
@@ -23,6 +23,18 @@ def get_next_open_row(board, col):
             return r
 def print_board(board):
     print(np.flip(board,0))
+def wining_move(board, piece):
+    #check horizontal location for win
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+    #check vertical location for win
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+
 
 board = create_board()
 print_board(board)
@@ -38,6 +50,9 @@ while not game_over:
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
 
+            if wining_move(board, 1):
+                print("player1 wins!")
+                game_over = True
 
     # ask for player 2 input
     else:
